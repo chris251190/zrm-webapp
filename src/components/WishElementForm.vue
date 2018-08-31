cbcb697625dfeaaccc317a72d983266b7858acd6
christianlehr <christian_lehr.1@gmx.de>
2018-09-01 00:13:57 +0200

    add stepper

--- src/components/WishElementForm.vue
+++ src/components/WishElementForm.vue
@@ -1,22 +1,74 @@
 <template>
-    <v-form v-model="valid">
-        {{wishelementquestion}}
-        <v-text-field v-for="(wishelement, index) in wishelements"
-                      :key="index"
-                      v-model="email"
-                      :rules="emailRules"
-                      :label="wishelement"
-                      required/>
-    </v-form>
+    <div>
+        <v-card class="mb-3">
+            <v-card-text>
+                <div>{{ typeof steps }}</div>
+
+                <v-text-field
+                        :value="steps"
+                        hint="This demo has a maximum of 6 steps"
+                        label="# of steps"
+                        max="6"
+                        min="1"
+                        persistent-hint
+                        type="number"
+                        @input="onInput"
+                ></v-text-field>
+            </v-card-text>
+        </v-card>
+        <v-stepper v-model="e1">
+            <v-stepper-header>
+                <template v-for="n in steps">
+                    <v-stepper-step
+                            :complete="e1 > n"
+                            :key="`${n}-step`"
+                            :step="n"
+                            editable
+                    >
+                        Step {{ n }}
+                    </v-stepper-step>
+
+                    <v-divider
+                            v-if="n !== steps"
+                            :key="n"
+                    ></v-divider>
+                </template>
+            </v-stepper-header>
+
+            <v-stepper-items>
+                <v-stepper-content
+                        v-for="n in steps"
+                        :key="`${n}-content`"
+                        :step="n"
+                >
+                    <v-form v-model="valid">
+                        {{wishelementquestion}}
+                        <v-text-field v-for="(wishelement, index) in wishelements"
+                                      :key="index"
+                                      v-model="email"
+                                      :rules="emailRules"
+                                      :label="wishelement"
+                                      required/>
+                    </v-form>
+
+                    <v-btn
+                            color="primary"
+                            @click="nextStep(n)"
+                    >
+                        Continue
+                    </v-btn>
+
+                    <v-btn flat>Cancel</v-btn>
+                </v-stepper-content>
+            </v-stepper-items>
+        </v-stepper>
+    </div>
 
 </template>
 
 <script>
     export default {
         name: 'WishElementForm',
-        props: {
-            msg: String
-        },
         data() {
             return {
                 wishelementquestion: 'Welches Wunschelement hat Eigenschaften, die dir dabei helfen, deine Absicht umzusetzen?',
@@ -28,7 +80,29 @@
                     'Fantasiefigur',
                     'Fahrzeug',
                     'Anderes'
-                ]
+                ],
+                e1: 1,
+                steps: 2
+            }
+        },
+        watch: {
+            steps(val) {
+                if (this.e1 > val) {
+                    this.e1 = val
+                }
+            }
+        },
+
+        methods: {
+            onInput(val) {
+                this.steps = parseInt(val)
+            },
+            nextStep(n) {
+                if (n === this.steps) {
+                    this.e1 = 1
+                } else {
+                    this.e1 = n + 1
+                }
             }
         }
     }
