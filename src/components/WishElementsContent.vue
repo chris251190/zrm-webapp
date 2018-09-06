23f7d02c412b388279ec8d44cb545e7337090d52
christianlehr <christian_lehr.1@gmx.de>
2018-09-06 22:57:35 +0200

    add vuex store to application

--- src/components/WishElementsContent.vue
+++ src/components/WishElementsContent.vue
@@ -61,6 +61,25 @@
 <script>
     import WishElementsIdeasSection from "./WishElementsIdeasSection";
     import WishElementsForm from "./WishElementsForm";
+    import Vue from 'vue'
+    import Vuex from 'vuex'
+
+    Vue.use(Vuex)
+
+    const store = new Vuex.Store({
+        state: {
+            count: 0
+        },
+        mutations: {
+            increment (state) {
+                state.count++
+            }
+        }
+    })
+
+    store.commit('increment')
+
+    console.log(store.state.count) // -> 1
 
     export default {
         name: 'wish-elements-content',
